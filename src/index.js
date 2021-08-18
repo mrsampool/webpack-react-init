2031c57150ea4544b2bf75a2a6a46eac3443f7d1
Sam Pool <sambpool@gmail.com>
2021-08-18 08:37:39 -0600

    project init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App';
+import './index.css';
+
+// Opt-in to Webpack hot module replacement
+if (module.hot) module.hot.accept()
+
+ReactDOM.render(<App/>, document.getElementById('app'));
