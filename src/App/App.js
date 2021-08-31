8280b2e26fa8a4d5e7ea937b18f1783fd621b9be
Sam Pool <sambpool@gmail.com>
2021-08-31 08:08:54 -0600

    extract UserInput to seperate component

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,14 @@ import { apiResults } from '../testData/apiResults';
 import { processListings } from '../utilities/dataUtilities';
 //import './App.css';
 import { Summary } from '../components/Summary/Summary';
+import {UserInput} from "../components/UserInput/UserInput";
 
 export const App = () => {
   return(
-    <div>
+    <div id={'App'}>
       <h1 className='logo'>ZipCost</h1>
-      <div className='d-flex flex-wrap'>
+      <UserInput/>
+      <div id={'Results'} className='d-flex flex-wrap'>
         { processListings(apiResults).map(zip => <Summary zip={zip}/> ) }
       </div>
     </div>
---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,9 @@
+//React
+import React from 'react'; 
+
+//Stylesheet
+import './UserInput.css' 
+
+export const UserInput = (props) =>{
+  return <div id={'UserInput'}/>;
+};
