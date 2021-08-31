90f651aa720a1f2b16acc1225c63a3db0075fcb0
Sam Pool <sambpool@gmail.com>
2021-08-31 08:11:48 -0600

    extract Results to seperate component

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { apiResults } from '../testData/apiResults';
-import { processListings } from '../utilities/dataUtilities';
-//import './App.css';
-import { Summary } from '../components/Summary/Summary';
+
+// Sub-Components
 import {UserInput} from "../components/UserInput/UserInput";
+import {Results} from "../components/Results/Results";
+
+// Style Sheet
+//import './App.css';
 
 export const App = () => {
   return(
     <div id={'App'}>
       <h1 className='logo'>ZipCost</h1>
       <UserInput/>
-      <div id={'Results'} className='d-flex flex-wrap'>
-        { processListings(apiResults).map(zip => <Summary zip={zip}/> ) }
-      </div>
+      <Results/>
     </div>
   )
 };
---
+++ src/components/Results/Results.js
@@ -0,0 +1,16 @@
+//React
+import React from 'react'; 
+
+//Stylesheet
+import './Results.css'
+import {processListings} from "../../utilities/dataUtilities";
+import {apiResults} from "../../testData/apiResults";
+import {Summary} from "../Summary/Summary";
+
+export const Results = (props) =>{
+  return(
+    <div id={'Results'} className='d-flex flex-wrap'>
+      { processListings(apiResults).map(zip => <Summary zip={zip}/> ) }
+    </div>
+  )
+};
